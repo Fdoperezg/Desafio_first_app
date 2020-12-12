a4cdfffd72d4622e0c0f1e1a12ccc4a801a101ed
Fernando <fp.naldo@gmail.com>
2020-12-12 11:48:00 -0300

    Segundo commit first_app

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'pages/one'
+  get 'pages/two'
+
+  #Con la instrucción siguiente se define la página de inicio
+  root 'pages#one'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
